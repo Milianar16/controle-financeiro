fb2580aaea428608f8d65f7178fbecc558a4f11f
Milianar16 <miliana.rocha16@gmail.com>
2023-08-02 14:39:03 -0300

    primeiro commit

---
+++ src/components/Header/styles.js
@@ -0,0 +1,17 @@
+import  styled  from "styled-components";
+
+export const Container =styled.div`
+height: 150px;
+text-align:center;
+background:#4682B4;
+background-image: url()
+
+
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+ padding-top: 20px;
+ color:#F0F8FF;
+`;
